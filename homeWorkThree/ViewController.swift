df9a855662e283238db049e46e7cabe55c0aefae
Kruksen <123827226+Kruksen@users.noreply.github.com>
2023-02-10 01:50:28 +0300

    finished

--- homeWorkThree/ViewController.swift
+++ homeWorkThree/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redLable: UILabel!
+    @IBOutlet weak var greenLable: UILabel!
+    @IBOutlet weak var blueLable: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        colorView.layer.cornerRadius = 15
+        
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+            )
+        
+        redLable.text = String(format: "%.2f", redSlider.value)
+        greenLable.text = String(format: "%.2f", greenSlider.value)
+        blueLable.text = String(format: "%.2f", blueSlider.value)
     }
 
-
+    @IBAction func sliderAction(_ sender: UISlider) {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+            )
+        
+        redLable.text = String(format: "%.2f", redSlider.value)
+        greenLable.text = String(format: "%.2f", greenSlider.value)
+        blueLable.text = String(format: "%.2f", blueSlider.value)
+    }
 }
 
