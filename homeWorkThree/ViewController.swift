f93982754f12306cac3134f65a6c523b701b3fd4
Kruksen <123827226+Kruksen@users.noreply.github.com>
2023-02-09 22:00:12 +0300

    Initial Commit

---
+++ homeWorkThree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWorkThree
+//
+//  Created by Алексей on 09.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
